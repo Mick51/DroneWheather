137c837a223b364dbd4863cee2c38143ff1eb3f6
Mikael <naudclick.informatik@gmail.com>
2026-06-25 22:26:02 +0200

    Fix bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.secrets)
 }
 
 android {
